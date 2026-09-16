f1e0124edc309796c42846e1664594caccc8c827
Din Barbershop <you@example.com>
2026-09-16 22:13:51 +0800

    style: fix squished layout of mobile checkout bar by preventing text wrapping and adjusting flex behavior

---
+++ scratch_fix_css_bar.js
@@ -0,0 +1,10 @@
+﻿const fs = require('fs');
+let css = fs.readFileSync('public/css/index.css', 'utf8');
+
+css = css.replace(
+  'text-transform: uppercase;',
+  'text-transform: uppercase;\n  white-space: nowrap;'
+);
+
+fs.writeFileSync('public/css/index.css', css);
+console.log("Added white-space nowrap to checkout-label.");
