54478df58fa9498d69b1730b3f9d7aec56b81b08
Din Barbershop <you@example.com>
2026-09-16 21:59:39 +0800

    style: force webkit mask for glowing border and reduce rgb line thickness to 1px

---
+++ scratch_fix_avatar_thickness.js
@@ -0,0 +1,10 @@
+﻿const fs = require('fs');
+let css = fs.readFileSync('public/css/index.css', 'utf8');
+
+css = css.replace(/top: -2px;/g, 'top: -1px;');
+css = css.replace(/left: -2px;/g, 'left: -1px;');
+css = css.replace(/right: -2px;/g, 'right: -1px;');
+css = css.replace(/bottom: -2px;/g, 'bottom: -1px;');
+
+fs.writeFileSync('public/css/index.css', css);
+console.log("Changed profile-avatar-wrapper to 1px border.");
---
+++ scratch_fix_border_thickness.js
@@ -0,0 +1,11 @@
+﻿const fs = require('fs');
+let css = fs.readFileSync('public/css/index.css', 'utf8');
+
+// Change top/left/right/bottom from -1px to 0
+css = css.replace(/top: -1px;/g, 'top: 0;');
+css = css.replace(/left: -1px;/g, 'left: 0;');
+css = css.replace(/right: -1px;/g, 'right: 0;');
+css = css.replace(/bottom: -1px;/g, 'bottom: 0;');
+
+fs.writeFileSync('public/css/index.css', css);
+console.log("Changed pseudo elements to inset 0 for true 1px border.");
