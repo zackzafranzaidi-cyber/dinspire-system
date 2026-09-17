102e70263a352d621258b19d907692a8c1cf796a
Din Barbershop <you@example.com>
2026-09-17 12:15:41 +0800

    style: fix various dark mode contrast issues, change intro to black, and thin rgb border

--- public/customer/sw.js
+++ public/customer/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dinspire-pwa-v15';
+const CACHE_NAME = 'dinspire-pwa-v16';
 const urlsToCache = [
   '/',
   '/index.html',
---
+++ scratch_bump_sw_v16.js
@@ -0,0 +1,5 @@
+﻿const fs = require('fs');
+let sw = fs.readFileSync('public/customer/sw.js', 'utf8');
+sw = sw.replace(/dinspire-pwa-v\d+/, 'dinspire-pwa-v16');
+fs.writeFileSync('public/customer/sw.js', sw);
+console.log("Bumped SW to v16");
